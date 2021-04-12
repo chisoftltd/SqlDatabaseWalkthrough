a7c2c1505ee1b3cba65fa2362e9e08dc7df0c88f
chisoftltd <chisoftltd@gmail.com>
2021-04-12 18:35:32 +0200

    Add project files.

---
+++ SampleDatabaseWalkthrough/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace SampleDatabaseWalkthrough
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.BtnGetAll = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGetAll
+            // 
+            this.BtnGetAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGetAll.Location = new System.Drawing.Point(340, 12);
+            this.BtnGetAll.Name = "BtnGetAll";
+            this.BtnGetAll.Size = new System.Drawing.Size(152, 45);
+            this.BtnGetAll.TabIndex = 0;
+            this.BtnGetAll.Text = "Get All Records";
+            this.BtnGetAll.UseVisualStyleBackColor = true;
+            this.BtnGetAll.Click += new System.EventHandler(this.BtnGetAll_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(717, 270);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student ID";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(160, 87);
+            this.txtStudentID.Multiline = true;
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(411, 36);
+            this.txtStudentID.TabIndex = 3;
+            this.txtStudentID.Validated += new System.EventHandler(this.txtStudentID_Validated);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(594, 87);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(172, 36);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnGetAll);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGetAll;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ SampleDatabaseWalkthrough/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SampleDatabaseWalkthrough
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BindingSource binder = new BindingSource();
+
+        public string conString { get; private set; }
+        public int studentIdValue { get; private set; }
+        public SqlCommand cmd { get; private set; }
+        public SqlDataAdapter adapter { get; private set; }
+        public DataTable dataTable { get; private set; }
+
+        private void BtnGetAll_Click(object sender, EventArgs e)
+        {
+            conString = Properties.Settings.Default.SDB;
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                cmd = new SqlCommand("NamesAndCourses", con);
+
+                adapter = new SqlDataAdapter(cmd);
+
+                dataTable = new DataTable();
+
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = binder;
+
+                binder.DataSource = dataTable;
+
+                //MessageBox.Show("All OK");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            conString = Properties.Settings.Default.SDB;
+            
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                cmd = new SqlCommand("GetStudentByID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("GetStudentID", studentIdValue));
+
+                adapter = new SqlDataAdapter(cmd);
+
+                dataTable = new DataTable();
+
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = binder;
+
+                binder.DataSource = dataTable;
+
+            }
+        }
+
+        private void txtStudentID_Validated(object sender, EventArgs e)
+        {
+            if (int.Parse(txtStudentID.Text) == 0 || int.Parse(txtStudentID.Text).ToString() == null)
+            {
+                MessageBox.Show("Enter Student ID");
+            }
+            else
+            {
+                btnSearch.Enabled = true;
+                studentIdValue = int.Parse(txtStudentID.Text);
+            }
+        }
+    }
+}
