4f2e1ed507775c3b95f74cbc7dd9645871e07462
chisoftltd <chisoftltd@gmail.com>
2021-04-12 19:09:09 +0200

    Coding for the Update

--- SampleDatabaseWalkthrough/Form1.Designer.cs
+++ SampleDatabaseWalkthrough/Form1.Designer.cs
@@ -29,12 +29,17 @@ namespace SampleDatabaseWalkthrough
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.BtnGetAll = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtStudentID = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
+            this.btnGetStudentsTable = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.txtGiven = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,18 +58,19 @@ namespace SampleDatabaseWalkthrough
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(49, 138);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(717, 270);
+            this.dataGridView1.Size = new System.Drawing.Size(717, 213);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // label1
             // 
@@ -97,11 +103,60 @@ namespace SampleDatabaseWalkthrough
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
+            // btnGetStudentsTable
+            // 
+            this.btnGetStudentsTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetStudentsTable.Location = new System.Drawing.Point(340, 358);
+            this.btnGetStudentsTable.Name = "btnGetStudentsTable";
+            this.btnGetStudentsTable.Size = new System.Drawing.Size(152, 36);
+            this.btnGetStudentsTable.TabIndex = 5;
+            this.btnGetStudentsTable.Text = "Get Students Table";
+            this.btnGetStudentsTable.UseVisualStyleBackColor = true;
+            this.btnGetStudentsTable.Click += new System.EventHandler(this.btnGetStudentsTable_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(49, 400);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Location = new System.Drawing.Point(267, 400);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(100, 20);
+            this.txtFamily.TabIndex = 7;
+            // 
+            // txtGiven
+            // 
+            this.txtGiven.Location = new System.Drawing.Point(491, 400);
+            this.txtGiven.Name = "txtGiven";
+            this.txtGiven.Size = new System.Drawing.Size(100, 20);
+            this.txtGiven.TabIndex = 8;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(691, 397);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtGiven);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnGetStudentsTable);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtStudentID);
             this.Controls.Add(this.label1);
@@ -123,6 +178,11 @@ namespace SampleDatabaseWalkthrough
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtStudentID;
         private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnGetStudentsTable;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.TextBox txtGiven;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
 
--- SampleDatabaseWalkthrough/Form1.cs
+++ SampleDatabaseWalkthrough/Form1.cs
@@ -19,6 +19,10 @@ namespace SampleDatabaseWalkthrough
         public SqlCommand cmd { get; private set; }
         public SqlDataAdapter adapter { get; private set; }
         public DataTable dataTable { get; private set; }
+        public DataGridViewRow selectRow { get; private set; }
+        public string sID { get; private set; }
+        public string fName { get; private set; }
+        public string gName { get; private set; }
 
         private void BtnGetAll_Click(object sender, EventArgs e)
         {
@@ -74,6 +78,63 @@ namespace SampleDatabaseWalkthrough
             {
                 btnSearch.Enabled = true;
                 studentIdValue = int.Parse(txtStudentID.Text);
+            }
+        }
+
+        private void btnGetStudentsTable_Click(object sender, EventArgs e)
+        {
+            conString = Properties.Settings.Default.SDB;
+
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                cmd = new SqlCommand("AllStudents", con);
+                
+                adapter = new SqlDataAdapter(cmd);
+
+                dataTable = new DataTable();
+
+                adapter.Fill(dataTable);
+
+                dataGridView1.DataSource = binder;
+
+                binder.DataSource = dataTable;
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            selectRow = dataGridView1.Rows[e.RowIndex];
+            sID = selectRow.Cells[0].Value.ToString();
+
+            fName = selectRow.Cells[1].Value.ToString();
+            gName = selectRow.Cells[2].Value.ToString();
+
+            txtID.Text = sID;
+            txtFamily.Text = fName;
+            txtGiven.Text = gName;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            studentIdValue = int.Parse(txtID.Text);
+            fName = txtFamily.Text;
+            gName = txtGiven.Text;
+
+
+            conString = Properties.Settings.Default.SDB;
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                cmd = new SqlCommand("UpdateStudentsTable", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@sID", studentIdValue));
+                cmd.Parameters.Add(new SqlParameter("@Fam", fName));
+                cmd.Parameters.Add(new SqlParameter("@Giv", gName));
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Updated:");
             }
         }
     }
